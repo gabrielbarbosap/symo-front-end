87a82ffdeae12fb46c41a5e6d0deaee7a6d5c833
mareanalitica <mareanalitica@gmail.com>
2025-02-08 16:48:28 -0300

    feat: add theme config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'symo';
+  constructor(private themeService: ThemeService) {}
+
+  toggleTheme(isDark: boolean) {
+    this.themeService.setDarkTheme(isDark);
+  }
 }
