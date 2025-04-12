c2f5a4472976c5bac5ce26c498d323bb7b415ae2
Anderson Feitosa <andersonmfjr@gmail.com>
2025-04-12 09:15:21 -0300

    refactor: auth routes

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -3,7 +3,10 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('token');
 
-  if (!req.url.includes('/auth') && !req.url.includes('/signup')) {
+  const authRoutes = ['/auth', '/signup'];
+  const isAuthRoute = authRoutes.some((route) => req.url.includes(route));
+
+  if (!isAuthRoute) {
     const authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`),
     });
