c252a64f3a60b633905954fbfbadb4b420337cd3
Anderson Feitosa <andersonmfjr@gmail.com>
2025-04-23 09:26:50 -0300

    feat: update api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment.model';
 
 export const environment: Environment = {
   production: false,
-  apiUrl: 'http://167.86.108.55:3333/api',
+  apiUrl: 'https://api2.symo.com.br/api',
 };
